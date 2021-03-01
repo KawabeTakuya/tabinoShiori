67994c3da54e093083bf8a25599f6d8e5960cf83
“KawabeTakuya” <kwb5223@gmail.com>
2021-03-01 18:03:58 +0900

    Initial Commit

---
+++ tabinoShiori/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabinoShiori
+//
+//  Created by 川邉拓哉 on 2021/03/01.
+//  Copyright © 2021 川邉拓哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
